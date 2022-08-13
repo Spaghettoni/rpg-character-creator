fb827771b5c141d53507169c4a9fc4491a79b514
Spaghettoni <kunzo6@gmail.com>
2022-08-13 15:11:55 +0200

    separate frontend & backend

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+    useEffect(() => {
+        fetch('http://localhost:8000/hello')
+            .then(async (response) => {
+                console.log('response:', response);
+                const body = await response.json();
+                console.log('body:', body)
+            })
+            .catch(err => {
+                console.log('err:', err);
+            })
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
