e09b46b03b2c712906c66625f0733e7881083679
Spaghettoni <kunzo6@gmail.com>
2022-08-15 22:26:42 +0200

    apollo-client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,31 @@
 import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@apollo/client';
+import {GET_USERS} from "./queries/user";
+
 
 const App = () => {
 
+    const { loading, error, data } = useQuery(GET_USERS);
+
     useEffect(() => {
-        fetch('http://localhost:8000/hello')
-            .then(async (response) => {
-                console.log('response:', response);
-                const body = await response.json();
-                console.log('body:', body)
-            })
-            .catch(err => {
-                console.log('err:', err);
-            })
+
     }, []);
+
+    if (loading) return <p>Loading...</p>;
+
+    if (error) return <p>Error :(</p>;
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                {data.allUsers.map((user: any, index: number) => (
+                    <p key={index}>
+                        User {index} : {user.email}
+                    </p>
+                ))}
             </header>
         </div>
     );
